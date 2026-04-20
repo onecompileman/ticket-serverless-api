dc71c0d2f04ca1eeb7c0aaa8b83b3e10d5554851
Stephen Vinuya <stephen.vinuya@auditmacs.com>
2026-04-20 20:01:33 +0800

    feat: updated

--- src/libs/http/handler.ts
+++ src/libs/http/handler.ts
@@ -1,8 +1,23 @@
 import middy from '@middy/core';
 import httpCors from '@middy/http-cors';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const baseHandler = (handler: any) =>
-    middy(handler)
+    middy(async (event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> => {
+        if (event.httpMethod === 'OPTIONS') {
+            return {
+                statusCode: 204,
+                body: '',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Methods': '*',
+                },
+            };
+        }
+
+        return handler(event, context);
+    })
         .use(httpCors())
         .use(httpJsonBodyParser({ disableContentTypeError: true }));
